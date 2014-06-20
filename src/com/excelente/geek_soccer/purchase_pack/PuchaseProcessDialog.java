14f73f903c581a125ef971feace07c6b77c441a8
geeksoccer <excelente74@gmail.com>
2014-06-20 20:52:50 +0700

    Pop: Nid noi

--- src/com/excelente/geek_soccer/purchase_pack/PuchaseProcessDialog.java
+++ src/com/excelente/geek_soccer/purchase_pack/PuchaseProcessDialog.java
@@ -115,6 +115,61 @@ public class PuchaseProcessDialog {
 			Log.w(tag, "mHelper.startSetup() - fail!");
 		}
 	}
+	
+	public static void FullStatPurchase(final String productID){
+		if (!isSetup) return;
+		 
+		mHelper.queryInventoryAsync(new IabHelper.QueryInventoryFinishedListener() {
+			@Override
+			public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+				boolean blnSuccess = result.isSuccess();
+				boolean blnFail = result.isFailure();
+
+				Log.i(tag, "mHelper.queryInventoryAsync() ...");
+				Log.i(tag, "	- blnSuccess return " + String.valueOf(blnSuccess));
+				Log.i(tag, "	- blnFail return " + String.valueOf(blnFail));
+
+				if (!blnSuccess) return;
+				
+				Log.i(tag, "	- inv.hasPurchase()   = " + inv.hasPurchase(productID));
+				Log.i(tag, "	- inv.getPurchase()   = " + inv.getPurchase(productID));
+				Log.i(tag, "	- inv.hasDetails()    = " + inv.hasDetails(productID));
+				Log.i(tag, "	- inv.getSkuDetails() = " + inv.getSkuDetails(productID));
+				
+				purchaseOwned=null;
+				
+				if (!inv.hasPurchase(productID)){// return;
+					purChase(productID);
+				}else{
+					purchaseOwned = inv.getPurchase(productID);
+					ConsumePuchase(productID);
+					Log.i(tag, "	- inv.getPurchase() ...");
+					Log.i(tag, "		.getDeveloperPayload() = " + purchaseOwned.getDeveloperPayload());
+					Log.i(tag, "		.getItemType()         = " + purchaseOwned.getItemType());
+					Log.i(tag, "		.getOrderId()          = " + purchaseOwned.getOrderId());
+					Log.i(tag, "		.getOriginalJson()     = " + purchaseOwned.getOriginalJson());
+					Log.i(tag, "		.getPackageName()      = " + purchaseOwned.getPackageName());
+					Log.i(tag, "		.getPurchaseState()    = " + String.valueOf(purchaseOwned.getPurchaseState()));
+					Log.i(tag, "		.getPurchaseTime()     = " + String.valueOf(purchaseOwned.getPurchaseTime()));
+					Log.i(tag, "		.getSignature()        = " + purchaseOwned.getSignature());
+					Log.i(tag, "		.getSku()              = " + purchaseOwned.getSku());
+					Log.i(tag, "		.getToken()            = " + purchaseOwned.getToken());
+
+					if (!inv.hasDetails(productID)) return;
+
+					SkuDetails skuDetails = inv.getSkuDetails(productID);
+					Log.i(tag, "	- inv.getSkuDetails() ...");
+					Log.i(tag, "		.getDescription() = " + skuDetails.getDescription());
+					Log.i(tag, "		.getPrice()       = " + skuDetails.getPrice());
+					Log.i(tag, "		.getSku()         = " + skuDetails.getSku());
+					Log.i(tag, "		.getTitle()       = " + skuDetails.getTitle());
+					Log.i(tag, "		.getType()        = " + skuDetails.getType());
+				}
+
+				
+			}
+		});
+	}
 
 	public static void Query(final String productID){
 		if (!isSetup) return;
@@ -213,6 +268,39 @@ public class PuchaseProcessDialog {
 
 				if (!blnSuccess) return;
 
+				purchaseOwned = null;
+
+				Log.i(tag, "	- purchase ...");
+				Log.i(tag, "		.getDeveloperPayload() = " + purchase.getDeveloperPayload());
+				Log.i(tag, "		.getItemType()         = " + purchase.getItemType());
+				Log.i(tag, "		.getOrderId()          = " + purchase.getOrderId());
+				Log.i(tag, "		.getOriginalJson()     = " + purchase.getOriginalJson());
+				Log.i(tag, "		.getPackageName()      = " + purchase.getPackageName());
+				Log.i(tag, "		.getPurchaseState()    = " + String.valueOf(purchase.getPurchaseState()));
+				Log.i(tag, "		.getPurchaseTime()     = " + String.valueOf(purchase.getPurchaseTime()));
+				Log.i(tag, "		.getSignature()        = " + purchase.getSignature());
+				Log.i(tag, "		.getSku()              = " + purchase.getSku());
+				Log.i(tag, "		.getToken()            = " + purchase.getToken());
+			}
+		});
+	}
+	
+	public static void ConsumePuchase(final String productID){
+		if (!isSetup) return;
+		if (purchaseOwned == null) return;
+
+		mHelper.consumeAsync(purchaseOwned, new IabHelper.OnConsumeFinishedListener() {
+			@Override
+			public void onConsumeFinished(Purchase purchase, IabResult result) {
+				boolean blnSuccess = result.isSuccess();
+				boolean blnFail = result.isFailure();
+
+				Log.i(tag, "mHelper.consumeAsync() ...");
+				Log.i(tag, "	- blnSuccess return " + String.valueOf(blnSuccess));
+				Log.i(tag, "	- blnFail return " + String.valueOf(blnFail));
+
+				if (!blnSuccess) return;
+				purChase(productID);
 				purchaseOwned = null;
 
 				Log.i(tag, "	- purchase ...");
